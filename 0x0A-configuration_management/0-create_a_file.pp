8c4557a05813863f7e0612e12dd7cdb68097f847
Aminj66 <aminemmah@gmail.com>
2024-04-15 16:25:00 +0300

    firewall

--- 0x0A-configuration_management/0-create_a_file.pp
+++
@@ -1,9 +0,0 @@
-# This puppet file creates a file called school in /tmp folder
-
-file {'/tmp/school':
-ensure  => 'present',
-mode    => '0744',
-owner   => 'www-data',
-group   => 'www-data',
-content => 'I love Puppet',
-}
